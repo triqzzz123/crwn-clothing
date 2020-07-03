06eb7536b420045e9f9922065b6b98a4d47f71d3
triqzzz123 <62540743+triqzzz123@users.noreply.github.com>
2020-07-03 10:22:03 +0530

    updated my menu-item  to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+
+const Hats=()=>(
+<div>
+ <h1>HATS PAGE</h1> 
+</div>
+)
+
+
 
 function App() {
   return (
     <div>
-  <HomePage/>
+  
+    <Route exact path='/' component={HomePage} />
+     <Route  exact path='/hats' component={Hats}/>
+     
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss'
-const HomePage=()=>(
+const HomePage=(props)=>{
+  console.log(props)
+  return(
   <div className='homepage'>
    <Directory />
   </div>
 
-);
+)
+}
 
 export default HomePage;
